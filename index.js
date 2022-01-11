4cb8ac18b10c92a1a9ed9b32eb26a1f168ece0ed
Sun Woo <wonderle2@gmail.com>
2022-01-11 23:10:01 +0900

    add

---
+++ index.js
@@ -0,0 +1,62 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello First Vue!'
+    }
+});
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+});
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            {text: 'Text 1'},
+            {text: 'Text 2'},
+            {text: 'Text 3'}
+        ]
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!',
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Two Way Binding.',
+    }
+});
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  });
+  
+
+
